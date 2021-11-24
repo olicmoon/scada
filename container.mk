5b7bc22ea355591660d5b14a2ccb6fdbffb290b4
Olic Moon <peer2linux@gmail.com>
2021-11-24 05:16:28 -0500

    another one (#4)
    Co-authored-by: Olic <omoon@boweryfarming.com>

--- container.mk
+++ container.mk
@@ -1,10 +1,20 @@
-ifndef scada_dir
-scada_dir := .
+ifndef workdir
+workdir := .
 endif
+
+check_defined = $(strip $(foreach 1,$1, \
+				$(call __check_defined,$1,$(strip $(value 2)))))
+__check_defined = $(if $(value $1),, \
+				  $(error Undefined parameter: $1$(if $2, ($2))))
 
 build:
 	@echo "build containers"
+	@echo "workdir:" $(workdir)
 
-container_setup:
-	@echo "Setup contaienr for $(target)"
-	ln -f $(scada_dir)/docker-compose-$(target).yml $(scada_dir)/docker-compose.yml
+setup:
+	$(call check_defined, target)
+
+	$(info Setup container for $(target) environment)
+	$(info linking docker-compose-$(target).xml)
+	
+	@cd $(workdir) && ln -sf docker-compose-$(target).yml docker-compose.yml
